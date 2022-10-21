daa6f6c621fc693816b155753c1197e7b0b3cc41
Ogunniyi Veronica <86062530+Keevah9@users.noreply.github.com>
2022-10-21 21:30:47 +0100

    icansee

--- src/components/FormAction.jsx
+++ src/components/FormAction.jsx
@@ -11,7 +11,7 @@ export default function FormAction({
                 (
                 <button
                     type={action}
-                    className="group relative w-full flex justify-center py-4 px-4 border border-transparent text-xl font-medium rounded-md text-white bg-[#67cd31] hover:bg-green-600 focus:outline-none focus:ring-2 font-bold focus:ring-offset-2 focus:ring-green-200 mt-12"
+                    className="group relative w-full flex justify-center py-4 px-4 border border-transparent text-xl rounded-md text-white bg-[#67cd31] hover:bg-green-600 focus:outline-none focus:ring-2 font-bold focus:ring-offset-2 focus:ring-green-200 mt-12"
                     onSubmit={handleSubmit} 
                 >
                     {text}
